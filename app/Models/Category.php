0410a831c67de9b8b82743a95e81b98b0b35020b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-11 14:22:52 +0700

    Model

---
+++ app/Models/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = "categories";
+    protected $primaryKey = "id";
+    protected $keyType = "string";
+    public $incrementing = false;
+    public $timestamps = false;
+}
